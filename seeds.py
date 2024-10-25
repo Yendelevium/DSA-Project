3752f3f2e1b7d29fbc5d0fa4637a6d5b5fc875e3
EliteGamer007 <150921203+EliteGamer007@users.noreply.github.com>
2024-10-25 19:05:14 +0530

    Update1 seeds.py

--- seeds.py
+++ seeds.py
@@ -3,11 +3,27 @@ from ADT import Game
 def getGames():
     return games
 games = [
-    Game("Hi", 10, ["HOE"], 12, 4),
-    Game("Hi", 10, ["HOE"], 12, 3),
-    Game("Hi", 10, ["HOE"], 12, 1),
-    Game("Hi", 10, ["HOE"], 12, 12),
-    Game("Hi", 10, ["HOE"], 12, 23),
-    Game("Hi", 10, ["HOE"], 12, 18),
-    Game("Hi", 10, ["HOE"], 12, 15),
+    Game("BattleField 1", 2000, ["Multiplayer"], 9, 19),
+    Game("Call Of Duty : Modern Warfare 2", 2500, ["Multiplayer"], 7, 20),
+    Game("Apex Legends", 0, ["Multiplayer"], 8, 21),
+    Game("Players Unknown Battlegrounds", 0, ["Multiplayer"], 9, 22),
+
+    Game("Horizon Forbidden West", 3500, ["Adventure"], 10, 23),
+    Game("Detroit:Become Human", 2500, ["Adventure"], 8, 24),
+    Game("Subnautica", 1500, ["Adventure"], 9, 25),
+    Game("Uncharted : Legacy Of Thieves", 2000, ["Adventure"], 9, 26),
+    Game("Red Dead Redemption 2", 1000, ["Adventure"], 10, 27),
+
+    Game("EAFC 25", 3500, ["Sports"], 3, 28),
+    Game("F124", 500, ["Sports"], 4, 29),
+    Game("Forza Horizon 5", 1800, ["Sports"], 8, 30),
+    Game("NFS Unbound", 1700, ["Sports"], 5, 31),
+    Game("NBA 2k25", 2800, ["Sports"], 6, 32),
+
+    Game("Baldur's Gate 3", 3000, ["RPG"], 9, 33),
+    Game("Witcher 3", 1700, ["RPG"], 10, 34),
+    Game("Diablo IV", 4000, ["RPG"], 4, 35),
+    Game("Elden Ring", 3000, ["RPG"], 9, 36),
+    Game("The Elder Scrolls V:Skyrim", 900, ["RPG"], 8, 37),
+
 ]
