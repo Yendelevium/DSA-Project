fd9a03c2181474673b91b52359ffead80c059d82
EliteGamer007 <150921203+EliteGamer007@users.noreply.github.com>
2024-10-27 21:04:45 +0530

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -2,30 +2,36 @@ from ADT import Game, Review
 from Trie import Trie
 from AVLTree import AVLTree
 from seeds import getGames
-from Graph import GameGraph
 from Minheap import MinHeap
+from MaxHeap import MaxHeap
+from Graph import GameGraph
+
 class GameStore:
     def __init__(self):
-        self.gameAVL=AVLTree()
-        self.gameHeapRating=MinHeap("rating")
-        self.gameHeapPrice=MinHeap("price")
-        self.gameGraph=GameGraph()
-        self.gameTrie=Trie()
+        self.gameAVL = AVLTree()
+        self.gameHeapRating = MinHeap("rating")
+        self.gameHeapPrice = MinHeap("price")
+        self.gameHeapRatingDesc = MaxHeap("rating")
+        self.gameHeapPriceDesc = MaxHeap("price")
+        self.gameGraph = GameGraph()
+        self.gameTrie = Trie()
         self.gameNameToId = {}
 
-    # AVL
+    # AVL Tree Stuff
     def getAllGames(self):
         return self.gameAVL.inorderTraversal(self.gameAVL.root)
 
-    def findGame(self,id):
-        return self.gameAVL.searchNode(self.gameAVL.root,id)
+    def findGame(self, id):
+        return self.gameAVL.searchNode(self.gameAVL.root, id)
 
-    def insertGame(self,game):
-        self.gameAVL.root=self.gameAVL.insertGame(self.gameAVL.root,game)
+    def insertGame(self, game):
+        self.gameAVL.root = self.gameAVL.insertGame(self.gameAVL.root, game)
         self.gameTrie.insert(game.name)
         self.gameNameToId[game.name] = game.id
         self.gameHeapPrice.insert(game)
+        self.gameHeapPriceDesc.insert(game)  # Add to descending price heap
         self.gameHeapRating.insert(game)
+        self.gameHeapRatingDesc.insert(game)  # Add to descending rating heap
         self.gameGraph.add_game(game, game.genre)
         for gen in game.genre:
             l = len(self.gameGraph.genre_map)
@@ -33,94 +39,53 @@ class GameStore:
                 self.gameGraph.genre_map[str(l + 1)] = gen
         return
 
-
     def gameNameSearch(self, gameName):
-        # Must return a list of games which match
-        # You can have a hashmap which maps the game names to the id, and you can search the ids in an AVL Tree
-        # Then return the list of games
-
-        # Use C to get the demo of more than one game
         matchedNames = self.gameTrie.search(gameName)
         if isinstance(matchedNames, bool) or not matchedNames:
-            # If no exact match, try autocomplete
             matchedNames = self.gameTrie.autocomplete(gameName)
             if not matchedNames:
-                # If no autocomplete results, try autocorrect
                 matchedNames = self.gameTrie.autocorrect(gameName)
         matchedGames = [self.findGame(self.gameNameToId[name]).id for name in matchedNames]
-        if (len(matchedNames)> 1):
+        if len(matchedNames) > 1:
             self.gameSelection([self.findGame(self.gameNameToId[name]) for name in matchedNames])
-        elif (len(matchedNames) == 1):
+        elif len(matchedNames) == 1:
             self.game_info(matchedGames[0])
         else:
             print("No games found with the entered name")
 
-    def sortRating(self,order):
+    def sortRating(self, order):
         if order == "ascending":
-            MinH = self.gameHeapRating
-            
-            sorted_games = MinH.extractSortedGames()
-            top_10_games = [
-                sorted_games[0],
-                sorted_games[1],
-                sorted_games[2],
-                sorted_games[3], 
-                sorted_games[4],
-                sorted_games[5],
-                sorted_games[6],
-                sorted_games[7],
-                sorted_games[8],
-                sorted_games[9]
-            ]
-            return top_10_games
-        return
+            return self.gameHeapRating.extractSortedGames()
+        elif order == "descending":
+            return self.gameHeapRatingDesc.extractSortedGames()
+        return []
 
-    def sortPrice(self,order):
+    def sortPrice(self, order):
         if order == "ascending":
-            MinH = self.gameHeapPrice
-            
-            sorted_games = MinH.extractSortedGames()
-            top_10_games = [
-                sorted_games[0],
-                sorted_games[1],
-                sorted_games[2],
-                sorted_games[3], 
-                sorted_games[4],
-                sorted_games[5],
-                sorted_games[6],
-                sorted_games[7],
-                sorted_games[8],
-                sorted_games[9]
-            ]
-            return top_10_games
-        return
+            return self.gameHeapPrice.extractSortedGames()
+        elif order == "descending":
+            return self.gameHeapPriceDesc.extractSortedGames()
+        return []
 
-    def getGenreGames(self,genre):
-        genbased = self.gameGraph.getGenreGames(genre)
-        gs = []
-        for game in genbased:
-            gs.append(game)
-        return gs
+    def getGenreGames(self, genre):
+        return self.gameGraph.getGenreGames(genre)
 
-    def gameSelection(self,games):
+    def gameSelection(self, games):
         print("Enter ID to know more about the following games")
         print("____________________________________")
         for i in games:
-            print(i.id,i.name)
-            print("Price: ",i.price)
-            print("Rating: ",i.rating)
-            print("Genre(s)")
-            for j in range(0,len(i.genre)):
-                print(i.genre[j], end=" ")
-            print()
+            print(i.id, i.name)
+            print("Price: ", i.price)
+            print("Rating: ", i.rating)
+            print("Genre(s):", " ".join(i.genre))
         print("____________________________________")
         search_id = int(input())
-        if(search_id <= id):
+        if search_id in self.gameNameToId.values():
             self.game_info(search_id)
         else:
             print("Invalid Game ID")
 
-    def game_info(self,gameChoice):
+    def game_info(self, gameChoice):
         retrievedGame = self.findGame(gameChoice)
         if retrievedGame is None:
             raise IndexError("Game doesn't exist")
@@ -129,87 +94,74 @@ class GameStore:
         print('Rating:', retrievedGame.rating)
         print('Genre(s):')
         for i in retrievedGame.genre:
-            print('\t',i)
+            print('\t', i)
 
         similar_games = self.gameGraph.similarGames(retrievedGame)
-        if similar_games:
+        if similar_games: 
             print("\nSimilar Games: ")
             for game_name in similar_games:
                 print("\t", game_name)
         else:
             print("\nNo Similar Games")
-        ch=input("Would you like to see the reviews?(Y/N)").upper()
-
-        # Todo : Adding reviews to the game
+        
+        ch = input("Would you like to see the reviews?(Y/N)").upper()
         if ch=="Y":
-            for i in retrievedGame.reviews:
-                print("Author:", i.author)
-                print("Review:", i.review)
+            for k,v in retrievedGame.reviews.items():
+                print("Author:", k)
+                print("Review:", v)
                 print()
             ch=input("Press any key to go back to homepage")
             return
         else:
             return
+
 # Driver Code
-gameStore=GameStore()
+gameStore = GameStore()
 games = getGames()
 for i in games:
     gameStore.insertGame(i)
-id=37 #We alr have 37 games in the tree
+id = 37  # We already have 37 games in the tree
+
 while True:
     print("Welcome to <Name>")
     print("Enter the corresponding option number")
-    print("1. View All Games") #AVL
-        # Then we can do the selection process again
-    print("2. Search for a game") #Trie
-        #Searching - Exact Match Search, Search by Substring,
-        #Auto-Correct - (Use a Trie-based Levenshtein Distance to find titles within a 1-2 character difference) - Trie
-
-        #AFTER SELECTING THE GAME
-        #Add review - AVL
-        #Show related games - Graph
-        #Show related Genres - Graph
-    print("3. Show top 10 Games by Price") #Heap
-        #Then we can do the selection process again
-    print("4. Show top 10 Games by Rating") #Heap
-        # Then we can do the selection process again
-    print("5. Show Games by Genre") #Graph
-        # Then we can do the selection process again
-    print("6. Add a Game") #AVL, Graph, Trie, Heap - Insert in all
+    print("1. View All Games")
+    print("2. Search for a game")
+    print("3. Show top 10 Games by Price")
+    print("4. Show top 10 Games by Rating")
+    print("5. Show Games by Genre")
+    print("6. Add a Game")
     print("Press Enter key to exit")
 
-    choice=input()
-    if choice=='':
+    choice = input()
+    if choice == '':
         print("Thank you for stopping by!")
         break
-    choice=int(choice)
-    if choice==1:
-        allGames=gameStore.getAllGames()
+    choice = int(choice)
+    
+    if choice == 1:
+        allGames = gameStore.getAllGames()
         gameStore.gameSelection(allGames)
 
-    elif choice==2:
+    elif choice == 2:
         print("Enter the name of the game")
-        gameSearch=input()
-        foundGames=gameStore.gameNameSearch(gameSearch)
-        if (foundGames):
-            print("Found Games:",foundGames)
+        gameSearch = input()
+        gameStore.gameNameSearch(gameSearch)
 
-    elif choice==3:
+    elif choice == 3:
         print("1. High to Low")
         print("2. Low to High")
-        ch=int(input())
-        if ch==1:
-            sortedGames=gameStore.sortPrice("descending")
+        ch = int(input())
+        if ch == 1:
+            sortedGames = gameStore.sortPrice("descending")
             gameStore.gameSelection(sortedGames)
-
-        elif ch==2:
+        elif ch == 2:
             sortedGames = gameStore.sortPrice("ascending")
             gameStore.gameSelection(sortedGames)
         else:
             raise IndexError("Choose correct value")
-            pass
 
-    elif choice==4:
+    elif choice == 4:
         print("1. High to Low")
         print("2. Low to High")
         ch = int(input())
@@ -221,9 +173,8 @@ while True:
             gameStore.gameSelection(sortedGames)
         else:
             raise IndexError("Choose correct value")
-            pass
 
-    elif choice==5:
+    elif choice == 5:
         print("Enter the number next to genre")
         for k, v in gameStore.gameGraph.genre_map.items():
             print(k, ":", v)
@@ -237,13 +188,12 @@ while True:
         else:
             print("Invalid genre choice. Please select a valid option.")
 
-    elif choice==6:
-        name=input("Game Name\t:")
-        price=float(input("Price\t:"))
-        rating=float(input("Rating\t:"))
-        genre=input("Genre(separated by space)\t:").split()
-        for gen in range(len(genre)):
-            genre[gen] = genre[gen][0].capitalize() + genre[gen][1:]
+    elif choice == 6:
+        name = input("Game Name\t:")
+        price = float(input("Price\t:"))
+        rating = float(input("Rating\t:"))
+        genre = input("Genre (separated by space)\t:").split()
+        genre = [g.capitalize() for g in genre]  # Capitalize each genre
         id += 1
         newGame = Game(name, price, genre, rating, id)
         gameStore.insertGame(newGame)
